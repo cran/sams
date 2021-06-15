3a6336d3fa964ad994c76ff1eeae84eb5186e876
David B. Dahl <dahl@stat.byu.edu>
2021-06-15 20:40:02 +0000

    version 0.4.2

---
+++ R/data.R
@@ -0,0 +1,101 @@
+#' Multivariate Independent Bernoulli Data (p = 6)
+#'
+#' Multivariate categorical data, generated as five clusters of independent variables with different
+#'   success probabilities.
+#'
+#' @name p6_bern
+NULL
+
+#' @rdname p6_bern
+"p6_bern_1"
+
+#' @rdname p6_bern
+"p6_bern_2"
+
+#' @rdname p6_bern
+"p6_bern_3"
+
+#' Multivariate Independent Bernoulli Data (p = 18)
+#'
+#' Multivariate categorical data, generated as five clusters of independent variables with different
+#'   success probabilities.
+#'
+#' @name p18_bern
+NULL
+
+#' @rdname p18_bern
+"p18_bern_1"
+
+#' @rdname p18_bern
+"p18_bern_2"
+
+#' @rdname p18_bern
+"p18_bern_3"
+
+#' Large Sample Multivariate Independent Bernoulli Data (p = 6)
+#'
+#' Multivariate categorical data, generated as five clusters of independent variables with different
+#'   success probabilities.
+#'
+#' @name p6_big_bern
+NULL
+
+#' @rdname p6_big_bern
+"p6_big_bern_1"
+
+#' @rdname p6_big_bern
+"p6_big_bern_2"
+
+#' @rdname p6_big_bern
+"p6_big_bern_3"
+
+#' Independent Multivariate Normal Data (p = 6)
+#'
+#' Multivariate continuous data, generated as five clusters of independent variables with different
+#'   means.
+#'
+#' @name p6_mvn
+NULL
+
+#' @rdname p6_mvn
+"p6_mvn_1"
+
+#' @rdname p6_mvn
+"p6_mvn_2"
+
+#' @rdname p6_mvn
+"p6_mvn_3"
+
+#' Independent Multivariate Normal Data (p = 18)
+#'
+#' Multivariate continuous data, generated as five clusters of independent variables with different
+#'   means.
+#'
+#' @name p18_mvn
+NULL
+
+#' @rdname p18_mvn
+"p18_mvn_1"
+
+#' @rdname p18_mvn
+"p18_mvn_2"
+
+#' @rdname p18_mvn
+"p18_mvn_3"
+
+#' Correlated Multivariate Normal Data (p = 18)
+#'
+#' Multivariate continuous data, generated as five clusters with an AR(1) correlation structure
+#'   determining the common covariance matrix for each observation.
+#'
+#' @name p18_corr_mvn
+NULL
+
+#' @rdname p18_corr_mvn
+"p18_corr_mvn_1"
+
+#' @rdname p18_corr_mvn
+"p18_corr_mvn_2"
+
+#' @rdname p18_corr_mvn
+"p18_corr_mvn_3"
